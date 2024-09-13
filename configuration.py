1d292e11bd4df60cf4f317f87c9dcaa1fca8ce37
Maria A Castillo <maricastillo0399@gmail.com>
2024-09-12 20:49:39 -0500

    fix: add new server URL

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-88fb59cf-aabe-44e2-9842-e755233eeceb.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-b8b6f1d3-58b0-48a5-afc9-63670fe47d8d.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 NEW_CLIENTE_KIT_PATH = "/api/v1/kits"
