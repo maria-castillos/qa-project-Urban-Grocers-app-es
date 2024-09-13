70049cc78b7acefd84c7bb72b2ade23211d5d1bd
Maria A Castillo <maricastillo0399@gmail.com>
2024-09-13 12:59:09 -0500

    new server URL

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-5a84410b-ffc9-40ea-8b07-1dfe94221314.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-bc323e69-e09c-4374-95c2-6c4d6e3bb4ea.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 NEW_CLIENTE_KIT_PATH = "/api/v1/kits"
